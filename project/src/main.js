8cb8d74534cad683da04230bc5a1bd89b4a5cf33
”Ann-Alex <annalexandrak@gmail.com>
2022-12-17 14:06:10 +0300

    cleared

--- project/src/main.js
+++ project/src/main.js
@@ -2,15 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-//import Axios from 'axios'
-//import {APIService} from './api/api'
-//import AxiosProvider from './api/axios.provider'
-
-//const axiosProvider = new AxiosProvider('http://localhost:3000')
-
-//Vue.prototype.$api = new APIService(axiosProvider)
-
-
 
 
 Vue.config.productionTip = false
